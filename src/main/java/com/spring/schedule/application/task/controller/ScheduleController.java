cefaa2c20c1f5abfd41a6e4fe1b446a1e153499d
lby9906 <lby9906@naver.com>
2025-08-02 18:09:46 +0900

    fix: scheduleController 주석 코드 추가

--- src/main/java/com/spring/schedule/application/task/controller/ScheduleController.java
+++ src/main/java/com/spring/schedule/application/task/controller/ScheduleController.java
@@ -23,27 +23,32 @@ public class ScheduleController {
     private final ScheduleWriteService scheduleWriteService;
     private final ScheduleReadService scheduleReadService;
 
+    //일정 생성
     @PostMapping
     public ScheduleResponse create(@RequestBody @Valid ScheduleRequest scheduleRequest) {
         return scheduleWriteService.create(scheduleRequest);
     }
 
+    //일정 전체 조회
     @GetMapping
     public List<ScheduleResponse> findAll(@RequestParam(required = false) String name) {
         return scheduleReadService.findAll(name);
     }
 
+    //일정 상세 조회
     @GetMapping("/{scheduleId}")
     public ScheduleByResponse findById(@PathVariable Long scheduleId) {
         return scheduleReadService.findById(scheduleId);
     }
 
+    //일정 수정
     @PatchMapping("/{scheduleId}")
     public ScheduleUpdateResponse update(@RequestBody @Valid ScheduleUpdateRequest request, @PathVariable Long scheduleId) {
         LocalDateTime now = LocalDateTime.now();
         return scheduleWriteService.update(request, scheduleId, now);
     }
 
+    //일정 삭제
     @DeleteMapping("/{scheduleId}")
     public String remove(@PathVariable Long scheduleId, @RequestParam String password) {
         return scheduleWriteService.remove(scheduleId, password);
